12c3ee8d29b8db116e4f3afa103393ffc8d5710f
Malte Muth <malte.muth+git-commit@googlemail.com>
2023-05-28 10:02:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Callout Maps
+
+TODO
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FunctionComponent<{}> = () => {
+  return <div className="App"></div>;
+};
+
+export default App;
